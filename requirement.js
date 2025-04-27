42a7b9769fc34b093ecde060d714a52670f5b8be
Saanidhya Chaturvedi <chaturvedisanidhya@gmail.com>
2025-04-27 13:15:26 +0530

    kuch bhi

---
+++ requirement.js
@@ -0,0 +1,83 @@
+ /*
+
+ Requirements
+
+ Book
+ - ISBN            - String
+ - Title           - String
+ - Author          - [Number]
+ - Language        - String  
+ - Publications    - Number
+ - NumofPages      - Number
+ - Categories      - [String]
+
+ Author
+ - id             - Number
+ - name           - String
+ - books          - [String]
+
+Publications
+ - id             - Number
+ - name           - String
+ - books          - [String]
+
+ -----APIs-----
+
+ Book
+ - GET
+  - to get all books
+  - to get specific books
+  - to get a list of books based on category
+  - to get a list of books based on author
+
+ - POST
+  - to add new book
+
+ - PUT
+  - to update book details
+  - to update/add new author
+  
+ - DELETE
+  - delete a book
+  - delete an author from the book
+
+Authors
+ - GET
+  - to get all authors
+  - to get specific author
+  - to get list of author based on a book
+
+ - POST
+ - to add new author
+  - to update/add new book     (isse yha kyu kra Post mei)
+
+ - PUT
+  - update author details 
+
+ - DELETE
+  - delete an author
+
+Publications
+ -GET
+  - to get all publication
+  - to get a specific publication
+  - to get a list of publication based on a book
+
+ -POST
+ - Add new Publication
+
+ -PUT
+ - update publication
+ - to update/add new book
+
+ -DELETE  
+  - delete a book from publication
+  - delete a publication
+
+
+
+
+
+
+ */
+console.log("My Name is Don");
